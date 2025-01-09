77e8af52d57ccf55446f9bdadc722bff0feda687
oille12 <oeg9176@naver.com>
2025-01-09 18:00:52 +0900

    infra_test_0109

---
+++ backend/infrapr/src/main/java/com/example/infrapr/InfraprApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraprApplication.class, args);
+	}
+
+}
---
+++ backend/infrapr/src/test/java/com/example/infrapr/InfraprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infrapr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
